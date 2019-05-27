97b40736efdfaaea518fe67c9683ea2156be1f60
Somesh Thakur <someshthakur33@gmail.com>
2019-05-27 11:33:06 +0530

    Add API Class

---
+++ js/API.js
@@ -0,0 +1,23 @@
+export class API {
+    constructor(url, key) {
+        this.url = url;
+        this.apiKey = key;
+        this.endpoints = {}
+    }
+
+    /*
+    * @param { A entity Obj } name
+     */
+    createEntity(enity) {
+        this.endpoints[enity] = this.createCRUDEndpoints(enity);
+    }
+
+    createCRUDEndpoints(name) {
+        let endpoints = {}
+        const resourceURL = `${this.url}/${name}`;
+        endpoints.getAll = () => fetch(`${resourceURL}`, { headers: { 'apiKey': `${this.apiKey}` } })
+            .then(res => res.json());
+        endpoints.getById = ({ uid_name = "unique_id", id }) => fetch(`${resourceURL}`, { headers: { 'apiKey': `${this.apiKey}`, uid_name: id } });
+        return endpoints;
+    }
+}
